cf9b2dfbea1f8d8d8224584eeb4a392e17eb64d4
Ambar Vélez <ambar.zavelez@gmail.com>
2020-03-08 18:36:52 -0400

    obtencion de datos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+import {data} from './data/pokemon/pokemon.js';
+
+console.log(data);
+
 function nextContainer(idNextContainer, idHideContainer) {
     
     document.getElementById(idNextContainer).style.display = "block";
     document.getElementById(idHideContainer).style.display ="none";
 
 }
+
